1ff142cda1bb1934c0d4da7d1986f8cb2e795c0c
fgengine <fgengine@gmail.com>
2017-08-30 12:54:59 +0300

    Support explicit optional on QJsonValue

--- Quickly/Json/IQJsonValue.swift
+++ Quickly/Json/IQJsonValue.swift
@@ -169,6 +169,12 @@ public func <<< < Type: IJsonValue >(left: inout Type, right: (QJson, String)) t
     left = (jsonValue as! Type)
 }
 
+public func <<< < Type: IJsonValue >(left: inout Type!, right: (QJson, String)) throws {
+    let source: Any? = try right.0.object(at: right.1)
+    let jsonValue: Any = try Type.fromJson(value: source)
+    left = (jsonValue as! Type)
+}
+
 public func <<< < Type: IJsonValue >(left: inout Type?, right: (QJson, String)) {
     if let source: Any? = try? right.0.object(at: right.1) {
         let jsonValue: Any? = try? Type.fromJson(value: source)
@@ -179,6 +185,22 @@ public func <<< < Type: IJsonValue >(left: inout Type?, right: (QJson, String)) 
 }
 
 public func <<< < Type: IJsonValue >(left: inout Type, right: (QJson, String, Type)) {
+    if let source: Any? = try? right.0.object(at: right.1) {
+        if let jsonValue: Any = try? Type.fromJson(value: source) {
+            if let value: Type = jsonValue as? Type {
+                left = value
+            } else {
+                left = right.2
+            }
+        } else {
+            left = right.2
+        }
+    } else {
+        left = right.2
+    }
+}
+
+public func <<< < Type: IJsonValue >(left: inout Type!, right: (QJson, String, Type)) {
     if let source: Any? = try? right.0.object(at: right.1) {
         if let jsonValue: Any = try? Type.fromJson(value: source) {
             if let value: Type = jsonValue as? Type {
@@ -248,6 +270,20 @@ public func <<< < EnumType: RawRepresentable >(left: inout EnumType, right: (QJs
     }
 }
 
+public func <<< < EnumType: RawRepresentable >(left: inout EnumType!, right: (QJson, String)) throws where EnumType.RawValue: IJsonValue {
+    let source: Any? = try right.0.object(at: right.1)
+    let jsonValue: Any = try EnumType.RawValue.fromJson(value: source)
+    if let rawValue: EnumType.RawValue = jsonValue as? EnumType.RawValue {
+        if let value: EnumType = EnumType(rawValue: rawValue) {
+            left = value
+        } else {
+            throw NSError(domain: QJsonErrorDomain, code: QJsonErrorCode.convert.rawValue, userInfo: nil)
+        }
+    } else {
+        throw NSError(domain: QJsonErrorDomain, code: QJsonErrorCode.convert.rawValue, userInfo: nil)
+    }
+}
+
 public func <<< < EnumType: RawRepresentable >(left: inout EnumType?, right: (QJson, String)) where EnumType.RawValue: IJsonValue {
     if let source: Any? = try? right.0.object(at: right.1) {
         if let jsonValue: Any = try? EnumType.RawValue.fromJson(value: source) {
@@ -265,6 +301,26 @@ public func <<< < EnumType: RawRepresentable >(left: inout EnumType?, right: (QJ
 }
 
 public func <<< < EnumType: RawRepresentable >(left: inout EnumType, right: (QJson, String, EnumType)) where EnumType.RawValue: IJsonValue {
+    if let source: Any? = try? right.0.object(at: right.1) {
+        if let jsonValue: Any = try? EnumType.RawValue.fromJson(value: source) {
+            if let rawValue: EnumType.RawValue = jsonValue as? EnumType.RawValue {
+                if let enumValue: EnumType = EnumType(rawValue: rawValue) {
+                    left = enumValue
+                } else {
+                    left = right.2
+                }
+            } else {
+                left = right.2
+            }
+        } else {
+            left = right.2
+        }
+    } else {
+        left = right.2
+    }
+}
+
+public func <<< < EnumType: RawRepresentable >(left: inout EnumType!, right: (QJson, String, EnumType)) where EnumType.RawValue: IJsonValue {
     if let source: Any? = try? right.0.object(at: right.1) {
         if let jsonValue: Any = try? EnumType.RawValue.fromJson(value: source) {
             if let rawValue: EnumType.RawValue = jsonValue as? EnumType.RawValue {
@@ -320,6 +376,10 @@ public func <<< (left: inout Date, right: (QJson, String, String)) throws {
     left = try right.0.date(at: right.1, formats: [right.2])
 }
 
+public func <<< (left: inout Date!, right: (QJson, String, String)) throws {
+    left = try right.0.date(at: right.1, formats: [right.2])
+}
+
 public func <<< (left: inout Date?, right: (QJson, String, String, Date?)) {
     if let date: Date = try? right.0.date(at: right.1, formats: [right.2]) {
         left = date
@@ -329,6 +389,10 @@ public func <<< (left: inout Date?, right: (QJson, String, String, Date?)) {
 }
 
 public func <<< (left: inout Date, right: (QJson, String, [String])) throws {
+    left = try right.0.date(at: right.1, formats: right.2)
+}
+
+public func <<< (left: inout Date!, right: (QJson, String, [String])) throws {
     left = try right.0.date(at: right.1, formats: right.2)
 }
 
@@ -393,6 +457,19 @@ public func <<< < ItemType: IJsonValue >(left: inout [ItemType], right: QJson) {
     }
 }
 
+public func <<< < ItemType: IJsonValue >(left: inout [ItemType]!, right: QJson) {
+    if let jsonArray: [Any] = right.array() {
+        left = jsonArray.flatMap({ (item: Any) -> ItemType? in
+            if let jsonValue: Any = try? ItemType.fromJson(value: item) {
+                return jsonValue as? ItemType
+            }
+            return nil
+        })
+    } else {
+        left = []
+    }
+}
+
 public func <<< < ItemType: IJsonValue >(left: inout [ItemType]?, right: QJson) {
     if let jsonArray: [Any] = right.array() {
         left = jsonArray.flatMap({ (item: Any) -> ItemType? in
@@ -407,6 +484,23 @@ public func <<< < ItemType: IJsonValue >(left: inout [ItemType]?, right: QJson) 
 }
 
 public func <<< < ItemType: IJsonValue >(left: inout [ItemType], right: (QJson, String)) {
+    if let array: [Any]? = try? right.0.array(at: right.1) {
+        if let jsonArray: [Any] = array {
+            left = jsonArray.flatMap({ (item: Any) -> ItemType? in
+                if let jsonValue: Any = try? ItemType.fromJson(value: item) {
+                    return jsonValue as? ItemType
+                }
+                return nil
+            })
+        } else {
+            left = []
+        }
+    } else {
+        left = []
+    }
+}
+
+public func <<< < ItemType: IJsonValue >(left: inout [ItemType]!, right: (QJson, String)) {
     if let array: [Any]? = try? right.0.array(at: right.1) {
         if let jsonArray: [Any] = array {
             left = jsonArray.flatMap({ (item: Any) -> ItemType? in
@@ -520,6 +614,26 @@ public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: 
     }
 }
 
+public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: inout [KeyType: ValueType]!, right: QJson) {
+    if let sourceDictionary = right.dictionary() {
+        var result: [KeyType: ValueType] = [:]
+        sourceDictionary.forEach({ (key: AnyHashable, value: Any) in
+            if let jsonKey: Any = try? KeyType.fromJson(value: key) {
+                if let safeKey: KeyType = jsonKey as? KeyType {
+                    if let jsonValue: Any = try? ValueType.fromJson(value: value) {
+                        if let safeValue: ValueType = jsonValue as? ValueType {
+                            result[safeKey] = safeValue
+                        }
+                    }
+                }
+            }
+        })
+        left = result
+    } else {
+        left = [:]
+    }
+}
+
 public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: inout [KeyType: ValueType]?, right: QJson) {
     if let sourceDictionary = right.dictionary() {
         var result: [KeyType: ValueType] = [:]
@@ -541,6 +655,30 @@ public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: 
 }
 
 public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: inout [KeyType: ValueType], right: (QJson, String)) {
+    if let dictionary: [AnyHashable: Any]? = try? right.0.dictionary(at: right.1) {
+        if let sourceDictionary: [AnyHashable: Any] = dictionary {
+            var result: [KeyType: ValueType] = [:]
+            sourceDictionary.forEach({ (key: AnyHashable, value: Any) in
+                if let jsonKey: Any = try? KeyType.fromJson(value: key) {
+                    if let safeKey: KeyType = jsonKey as? KeyType {
+                        if let jsonValue: Any = try? ValueType.fromJson(value: value) {
+                            if let safeValue: ValueType = jsonValue as? ValueType {
+                                result[safeKey] = safeValue
+                            }
+                        }
+                    }
+                }
+            })
+            left = result
+        } else {
+            left = [:]
+        }
+    } else {
+        left = [:]
+    }
+}
+
+public func <<< < KeyType: IJsonValue & Hashable, ValueType: IJsonValue >(left: inout [KeyType: ValueType]!, right: (QJson, String)) {
     if let dictionary: [AnyHashable: Any]? = try? right.0.dictionary(at: right.1) {
         if let sourceDictionary: [AnyHashable: Any] = dictionary {
             var result: [KeyType: ValueType] = [:]
--- QuicklyTests/QuicklyApiTests.swift
+++ QuicklyTests/QuicklyApiTests.swift
@@ -13,7 +13,7 @@ class QuicklyApiTests: XCTestCase {
         let provider: QApiProvider = QApiProvider(baseUrl: URL(string: "http://yandex.ru")!)
         let request: QApiRequest = QApiRequest(method: "GET")
         let response: QApiResponse = QApiResponse()
-        let query: IQApiQuery = provider.send(request: request, response: response) { (request: QApiRequest, response: QApiResponse) in
+        _ = provider.send(request: request, response: response) { (request: QApiRequest, response: QApiResponse) in
             expectation.fulfill()
         }
         self.wait(for: [expectation], timeout: 100)
