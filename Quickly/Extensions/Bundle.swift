cacf88f5dfb45a91e83c7ca4fbc86afd6e9e4e07
Alexander Trifonov <fgengine@gmail.com>
2019-03-18 13:35:34 +0300

    Refactoring & Stabilization

--- Quickly/Extensions/Bundle.swift
+++ Quickly/Extensions/Bundle.swift
@@ -4,6 +4,11 @@
 
 public extension Bundle {
     
+    public var isTestFlight: Bool {
+        get {
+            return self.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+        }
+    }
     public var version: String? {
         get {
             return self.object(forInfoDictionaryKey: "CFBundleVersion") as? String
