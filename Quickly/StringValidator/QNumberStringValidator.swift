8c8bc434f6c1ad1bd3a73804c0e91e5407bd9552
Alexander Trifonov <fgengine@gmail.com>
2019-04-30 10:25:36 +0300

    Refactoring & Stabilization

--- Quickly/StringValidator/QNumberStringValidator.swift
+++ Quickly/StringValidator/QNumberStringValidator.swift
@@ -4,21 +4,15 @@
 
 open class QNumberStringValidator : QLengthStringValidator {
 
-    open var formatter: NumberFormatter {
-        didSet { self.formatter.generatesDecimalNumbers = true }
-    }
     open var minimumValue: Decimal?
     open var maximumValue: Decimal?
 
     public init(
-        formatter: NumberFormatter,
         minimumValue: Decimal? = nil,
         maximumValue: Decimal? = nil,
         minimumLength: Int,
         maximumLength: Int? = nil
     ) {
-        self.formatter = formatter
-        self.formatter.generatesDecimalNumbers = true
         self.minimumValue = minimumValue
         self.maximumValue = maximumValue
         super.init(
@@ -30,13 +24,14 @@ open class QNumberStringValidator : QLengthStringValidator {
     open override func validate(_ string: String, complete: Bool) -> Bool {
         var valid = super.validate(string, complete: complete)
         if valid == true {
-            if let number = self.formatter.number(from: string) as? Decimal {
+            if let number = NSDecimalNumber.decimalNumber(from: string) {
+                let value = number as Decimal
                 if let maximumValue = self.maximumValue {
-                    valid = number <= maximumValue
+                    valid = value <= maximumValue
                 }
                 if valid == true && complete == true {
                     if let minimumValue = self.minimumValue {
-                        valid = number >= minimumValue
+                        valid = value >= minimumValue
                     }
                 }
             }
