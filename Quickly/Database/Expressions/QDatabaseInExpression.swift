33727a76d203cf05b76dc265584e1d508a04feb3
Alexander Trifonov <fgengine@gmail.com>
2019-11-22 11:24:40 +0300

    Refactoring & Stabilization

--- Quickly/Database/Expressions/QDatabaseInExpression.swift
+++ Quickly/Database/Expressions/QDatabaseInExpression.swift
@@ -30,4 +30,28 @@ public extension QDatabase {
         
     }
     
+    struct NotInExpression : IQDatabaseExpressable {
+        
+        public let column: QDatabase.Column
+        public let values: [IQDatabaseInputValue]
+        
+        public init(
+            _ column: QDatabase.Column,
+            _ values: [IQDatabaseInputValue]
+        ) {
+            self.column = column
+            self.values = values
+        }
+        
+        public func inputValues() -> [IQDatabaseInputValue] {
+            return self.values
+        }
+        
+        public func queryExpression() -> String {
+            let values = self.values.compactMap({ _ in return "?" })
+            return self.column.name + " NOT IN (" +  values.joined(separator: ", ") + ")"
+        }
+        
+    }
+    
 }
