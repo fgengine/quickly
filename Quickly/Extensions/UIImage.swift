4f1abeed97bee8df4ba1bfc8f89dd39f9ad8a057
Alexander Trifonov <fgengine@gmail.com>
2020-07-30 18:37:44 +0300

    Refactoring & Stabilization

--- Quickly/Extensions/UIImage.swift
+++ Quickly/Extensions/UIImage.swift
@@ -31,56 +31,80 @@ public extension UIImage {
         }
     }
     
-    func unrotate() -> UIImage? {
-        guard self.imageOrientation != .up else {
+    func unrotate(maxResolution: CGFloat) -> UIImage {
+        guard let imgRef = self.cgImage else {
             return self
         }
-        let size = self.size
-        let imageOrientation = self.imageOrientation
-        var transform = CGAffineTransform.identity;
+        let width = CGFloat(imgRef.width);
+        let height = CGFloat(imgRef.height);
+        var bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        var scaleRatio : CGFloat = 1
+        if width > maxResolution || height > maxResolution {
+            scaleRatio = min(maxResolution / bounds.size.width, maxResolution / bounds.size.height)
+            bounds.size.height = bounds.size.height * scaleRatio
+            bounds.size.width = bounds.size.width * scaleRatio
+        }
+        let orientation = self.imageOrientation
+        var transform = CGAffineTransform.identity
+        let imageSize = CGSize(width: CGFloat(imgRef.width), height: CGFloat(imgRef.height))
         switch imageOrientation {
-        case .down, .downMirrored:
-            transform = transform.translatedBy(x: size.width, y: size.height)
-            transform = transform.rotated(by: CGFloat.pi)
-        case .left, .leftMirrored:
-            transform = transform.translatedBy(x: size.width, y: 0)
-            transform = transform.rotated(by: CGFloat.pi / 2)
-        case .right, .rightMirrored:
-            transform = transform.translatedBy(x: 0, y: size.height)
-            transform = transform.rotated(by: -(CGFloat.pi / 2))
-        default:
-            break
+        case .up:
+            transform = CGAffineTransform.identity
+        case .upMirrored:
+            transform = CGAffineTransform(translationX: imageSize.width, y: 0.0)
+            transform = transform.scaledBy(x: -1.0, y: 1.0)
+        case .down:
+            transform = CGAffineTransform(translationX: imageSize.width, y: imageSize.height)
+            transform = transform.rotated(by: .pi)
+        case .downMirrored:
+            transform = CGAffineTransform(translationX: 0.0, y: imageSize.height)
+            transform = transform.scaledBy(x: 1.0, y: -1.0)
+        case .left:
+            let storedHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = storedHeight
+            transform = CGAffineTransform(translationX: 0.0, y: imageSize.width)
+            transform = transform.rotated(by: 3.0 * .pi / 2.0)
+        case .leftMirrored:
+            let storedHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = storedHeight
+            transform = CGAffineTransform(translationX: imageSize.height, y: imageSize.width)
+            transform = transform.scaledBy(x: -1.0, y: 1.0)
+            transform = transform.rotated(by: 3.0 * .pi / 2.0)
+        case .right:
+            let storedHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = storedHeight
+            transform = CGAffineTransform(translationX: imageSize.height, y: 0.0)
+            transform = transform.rotated(by: .pi / 2.0)
+        case .rightMirrored:
+            let storedHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = storedHeight
+            transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+            transform = transform.rotated(by: .pi / 2.0)
+        @unknown default:
+            fatalError()
         }
-        switch imageOrientation {
-        case .upMirrored, .downMirrored:
-            transform = transform.translatedBy(x: size.width, y: 0)
-            transform = transform.scaledBy(x: -1, y: 1)
-        case .leftMirrored, .rightMirrored:
-            transform = transform.translatedBy(x: size.height, y: 0)
-            transform = transform.scaledBy(x: -1, y: 1)
-        default:
-            break
+        UIGraphicsBeginImageContext(bounds.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return self
         }
-        guard let cgImage = self.cgImage else {
-            return nil
+        if orientation == .right || orientation == .left {
+            context.scaleBy(x: -scaleRatio, y: scaleRatio)
+            context.translateBy(x: -height, y: 0)
+        } else {
+            context.scaleBy(x: scaleRatio, y: -scaleRatio)
+            context.translateBy(x: 0, y: -height)
         }
-        UIGraphicsBeginImageContextWithOptions(size, false, scale)
-        defer {
-            UIGraphicsEndImageContext()
+        context.concatenate(transform)
+        context.draw(imgRef, in: CGRect(x: 0, y: 0, width: width, height: height))
+        guard let imageCopy = UIGraphicsGetImageFromCurrentImageContext() else {
+            return self
         }
-        if let context = UIGraphicsGetCurrentContext() {
-            context.concatenate(transform)
-            let rect: CGRect
-            switch imageOrientation {
-            case .left, .leftMirrored, .right, .rightMirrored: rect = CGRect(x: 0, y: 0, width: size.height, height: size.width)
-            default: rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-            }
-            context.draw(cgImage, in: rect)
-            if let image = context.makeImage() {
-                return UIImage(cgImage: image, scale: self.scale, orientation: .up)
-            }
-        }
-        return nil
+        UIGraphicsEndImageContext()
+        return imageCopy
     }
     
     func scaleTo(size: CGSize) -> UIImage? {
