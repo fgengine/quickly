ff499caeb49b9ac27601de146a33d7588276d7ce
Alexander Trifonov <fgengine@gmail.com>
2020-10-02 12:48:06 +0300

    Refactoring & Stabilization

--- Quickly/ViewControllers/Dialog/Background/QDialogBlurBackgroundView.swift
+++ Quickly/ViewControllers/Dialog/Background/QDialogBlurBackgroundView.swift
@@ -7,7 +7,7 @@ public final class QDialogBlurBackgroundView : QBlurView, IQDialogContainerBackg
     public weak var containerViewController: IQDialogContainerViewController?
 
     public required init() {
-        super.init(blurRadius: Const.hiddenBlurRadius)
+        super.init(blur: Const.hiddenBlur)
     }
 
     public required init?(coder: NSCoder) {
@@ -17,7 +17,7 @@ public final class QDialogBlurBackgroundView : QBlurView, IQDialogContainerBackg
     public override func setup() {
         super.setup()
 
-        self.blurRadius = Const.hiddenBlurRadius
+        self.blur = Const.hiddenBlur
         self.isHidden = true
     }
 
@@ -26,10 +26,10 @@ public final class QDialogBlurBackgroundView : QBlurView, IQDialogContainerBackg
             self.isHidden = false
             if animated == true {
                 UIView.animate(withDuration: Const.duration, delay: 0, options: [ .beginFromCurrentState ], animations: {
-                    self.blurRadius = Const.visibleBlurRadius
+                    self.blur = Const.visibleBlur
                 })
             } else {
-                self.blurRadius = Const.visibleBlurRadius
+                self.blur = Const.visibleBlur
             }
         }
     }
@@ -38,12 +38,12 @@ public final class QDialogBlurBackgroundView : QBlurView, IQDialogContainerBackg
         if isLast == true {
             if animated == true {
                 UIView.animate(withDuration: Const.duration, delay: 0, options: [ .beginFromCurrentState ], animations: {
-                    self.blurRadius = Const.hiddenBlurRadius
+                    self.blur = Const.hiddenBlur
                 }, completion: { (_) in
                     self.isHidden = true
                 })
             } else {
-                self.blurRadius = Const.hiddenBlurRadius
+                self.blur = Const.hiddenBlur
                 self.isHidden = true
             }
         }
@@ -58,8 +58,8 @@ private extension QDialogBlurBackgroundView {
     struct Const {
 
         static let duration: TimeInterval = 0.25
-        static let hiddenBlurRadius: CGFloat = 0
-        static let visibleBlurRadius: CGFloat = 20
+        static let hiddenBlur: UIBlurEffect? = nil
+        static let visibleBlur: UIBlurEffect? = UIBlurEffect(style: .dark)
 
     }
 
